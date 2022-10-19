275ccb2e77c23f29c824ee736094ae8a97ed8168
Tiger <tiger@Tigers-iMac-Pro.local>
2022-10-19 16:01:52 -0400

    modified address

--- controllers/front/AddressController.php
+++ controllers/front/AddressController.php
@@ -11,9 +11,14 @@
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to license@prestashop.com so we can send you a copy immediately.
- *
+ * Warsaw 44 (Miasto 44)
  * DISCLAIMER
- *
+ * olga oleksiy
+ * Chase Sui Wonders 
+ * anna miklosh
+ * anatoliy kotenyov
+ * Vsyo budet khorosho
+ * davai pozhenimsya
  * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
  * versions in the future. If you wish to customize PrestaShop for your
  * needs please refer to https://www.prestashop.com for more information.
