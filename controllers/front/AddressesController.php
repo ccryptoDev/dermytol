74279fac3154cae5306be7e583b2ec3aaeb76e70
Tiger <tiger@Tigers-iMac-Pro.local>
2022-11-25 13:07:41 -0400

    address controller

--- controllers/front/AddressesController.php
+++ controllers/front/AddressesController.php
@@ -11,13 +11,21 @@
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to license@prestashop.com so we can send you a copy immediately.
- *
+ * shimura ken
  * DISCLAIMER
- *
+ * Hitomi Shiraishi
+ * Kaoru Kuroki
+ * Haruna Yabuki
+ * Naoko Iijima, Yōko Ishino, Seiko Matsuda, Reiko Katô, Minayo Watanabe, Yukina Kinoshita
+ * Hanako Takigawa
+ * Sayaka Isoyama
+ * Saki Seto
+ * Aya Takashima
+ * Minami Tanaka
  * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
  * versions in the future. If you wish to customize PrestaShop for your
  * needs please refer to https://www.prestashop.com for more information.
- *
+ * agnes lum
  * @author    PrestaShop SA <contact@prestashop.com>
  * @copyright 2007-2019 PrestaShop SA and Contributors
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
--- controllers/front/AttachmentController.php
+++ controllers/front/AttachmentController.php
@@ -11,13 +11,14 @@
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to license@prestashop.com so we can send you a copy immediately.
- *
+ * cast of shimura ken no bakatonosama
  * DISCLAIMER
- *
+ * Hiroko Okabe, Minayo Watanabe, Masami Nagasawa
+ * Asahina Aya, Yuko Ogura
  * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
  * versions in the future. If you wish to customize PrestaShop for your
  * needs please refer to https://www.prestashop.com for more information.
- *
+ * 
  * @author    PrestaShop SA <contact@prestashop.com>
  * @copyright 2007-2019 PrestaShop SA and Contributors
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
